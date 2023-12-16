4f6da68d3603cd147f9d093fb1abda3c1f48f60b
Konstantin <kostyacherepanov1@gmail.com>
2023-12-17 01:15:02 +0700

    1

--- internal/app/storage/psql/psql_storage.go
+++ internal/app/storage/psql/psql_storage.go
@@ -104,12 +104,12 @@ func (storage *PsqURLlStorage) InsertOrder(ctx context.Context, login string, or
 		return gophermarterrors.ErrYouAlreadyLoadedOrder
 	}
 
-	go storage.GetStatusFromAccural(*userOrder)
+	go storage.GetStatusFromAccural(ctx, *userOrder)
 
 	return nil
 }
 
-func (storage *PsqURLlStorage) GetStatusFromAccural(order Order) {
+func (storage *PsqURLlStorage) GetStatusFromAccural(ctx context.Context, order Order) {
 	var wg sync.WaitGroup
 
 	orderJob := make(chan Order)
@@ -162,9 +162,7 @@ func (storage *PsqURLlStorage) GetStatusFromAccural(order Order) {
 
 	go func() {
 		for orderToUpdate := range result {
-			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 			storage.UpdateStatus(ctx, orderToUpdate, order.Login)
-			cancel()
 		}
 	}()
 
